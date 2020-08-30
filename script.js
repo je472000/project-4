ff7a5b63d0ded7be0f1131dbb85890e16a3d4d6f
James Ellis <jimfort64@yahoo.com>
2020-08-30 17:49:12 -0500

    initial data structure

---
+++ script.js
@@ -0,0 +1,44 @@
+const availableHours = [
+  {
+    time: "8am",
+    meeting: "",
+  },
+  {
+    time: "9am",
+    meeting: "",
+  },
+  {
+    time: "10am",
+    meeting: "",
+  },
+  {
+    time: "11am",
+    meeting: "",
+  },
+  {
+    time: "12pm",
+    meeting: "",
+  },
+  {
+    time: "1pm",
+    meeting: "",
+  },
+  {
+    time: "2pm",
+    meeting: "",
+  },
+  {
+    time: "3pm",
+    meeting: "",
+  },
+  {
+    time: "4pm",
+    meeting: "",
+  },
+  {
+    time: "5pm",
+    meeting: "",
+  },
+];
+
+
