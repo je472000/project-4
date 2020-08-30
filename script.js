32db1cc9659227849c6518da4783cb57c668f72f
James Ellis <jimfort64@yahoo.com>
2020-08-30 17:54:25 -0500

    more progress

--- script.js
+++ script.js
@@ -41,4 +41,21 @@ const availableHours = [
   },
 ];
 
+$(document).ready(function () {
+    // Let us set today's date at the top of the calendar
+    var calendarItems = JSON.parse(localStorage.getItem("myschedule"));
+  
+    // If there is nothing in local storage, start with a fresh calendar defined above
+    if (!calendarItems) {
+      calendarItems = availableHours;
+    }
+  
+    // Inject today's date to the top of the screen
+    document.getElementById("Today").innerHTML = moment(new Date()).format(
+      "MM/DD/YYYY h:mma"
+    );
+  
 
+
+    
+    });
